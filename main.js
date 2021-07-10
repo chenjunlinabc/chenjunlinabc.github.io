b65bbb1037bbfad7613313aec1e1b754fae94277
chenjunlin <3222391319@qq.com>
2021-07-10 19:50:20 +0800

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const searchData = document.querySelector("#search-data");
-const submitData = document.querySelector(".submit");
-
-submitData.onclick = function(){
-    const urlData = "https://www.baidu.com/s?wd="+ searchData.value;
-    window.location.href = urlData;
-    // window.open(urlData);
-}
-
